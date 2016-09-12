b6d18b35524ff0cc09a00f94bb167a819ba34a50
Eric D. Reichert <eric@reichertconsulting.com>
2016-09-11 22:05:13 -0400

    Added handlebars-iron support.  Built the first view template with a test view.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,32 @@
+extern crate env_logger;
 extern crate iron;
 
-use iron::prelude::*;
-use iron::status;
+use iron::prelude::{Chain, Iron, Request, Response};
+use iron::{Set,status};
+extern crate handlebars_iron as hbs;
+use std::error::Error;
 
 fn main() {
+    env_logger::init().unwrap();
+
+    let mut hbse = hbs::HandlebarsEngine::new();
+    hbse.add(Box::new(hbs::DirectorySource::new("./src/views/", ".hbs")));
+    if let Err(r) = hbse.reload() {
+        panic!("{:?}", r.description());
+    }
+
+    let mut chain = Chain::new(|_: &mut Request| {
+        let mut resp = Response::new();
+        resp.set_mut(hbs::Template::new("index", "".to_string())).set_mut(status::Ok);
+        Ok(resp)
+    });
+
+    chain.link_after(hbse);
+
     let port = 3000;
     let bind_addr = format!("localhost:{}", port);
-    let _server_guard = Iron::new(|_: &mut Request| {
-        Ok(Response::with((status::Ok, "Hello World!")))
-    }).http(bind_addr.as_str()).unwrap();
+    let _server_guard = Iron::new(chain).http(bind_addr.as_str()).unwrap();
 
     let version = include_str!("version.txt");
     println!("Running WLB v{} on port {}.", version, port)
 }
-
