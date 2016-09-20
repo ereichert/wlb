cf9542ecb844b07f0cde237f95459d6a679f30ca
Eric D. Reichert <eric@reichertconsulting.com>
2016-09-19 21:52:22 -0400

    Added support for displaying task history in the UI using rust_serialize and the each block helper of handlebars.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
+#![feature(custom_derive, plugin)]
+#![plugin(tojson_macros)]
+
 extern crate env_logger;
 extern crate iron;
 extern crate handlebars_iron as hbs;
 extern crate router;
 extern crate mount;
 extern crate staticfile;
+extern crate rustc_serialize;
 
 use iron::prelude::{Chain, Iron, Request, Response};
 use iron::{IronResult, status, Set};
@@ -15,9 +19,12 @@ use hbs::Watchable;
 use mount::Mount;
 use staticfile::Static;
 
+use rustc_serialize::json::{Json, ToJson};
+
 use std::sync::Arc;
 use std::error::Error;
 use std::path::Path;
+use std::collections::BTreeMap;
 
 fn main() {
     env_logger::init().unwrap();
@@ -54,7 +61,66 @@ fn main() {
 }
 
 fn home_handler(_: &mut Request) -> IronResult<Response> {
+    let data = make_test_records();
     let mut resp = Response::new();
-    resp.set_mut(hbs::Template::new("home", "".to_string())).set_mut(status::Ok);
+    resp.set_mut(hbs::Template::new("home", data)).set_mut(status::Ok);
     Ok(resp)
+}
+
+#[derive(ToJson)]
+struct TaskRecord<'a> {
+    date: &'a str,
+    start_time: &'a str,
+    end_time: &'a str,
+    project_name: &'a str,
+    description: &'a str,
+}
+
+fn make_test_records() -> BTreeMap<String, Vec<Json>> {
+    let mut data = BTreeMap::new();
+    data.insert("task_records".to_string(), vec![
+        TaskRecord {
+            date: "09/19/2016",
+            start_time: "06:58",
+            end_time: "07:45",
+            project_name: "TL",
+            description: "Research, reading, schedule",
+        }.to_json(),
+        TaskRecord {
+            date: "09/19/2016",
+            start_time: "07:45",
+            end_time: "09:30",
+            project_name: "TL",
+            description: "Development",
+        }.to_json(),
+        TaskRecord {
+            date: "09/19/2016",
+            start_time: "09:30",
+            end_time: "09:34",
+            project_name: "TL",
+            description: "standup",
+        }.to_json(),
+        TaskRecord {
+            date: "09/19/2016",
+            start_time: "09:34",
+            end_time: "09:45",
+            project_name: "TL",
+            description: "comms",
+        }.to_json(),
+        TaskRecord {
+            date: "09/19/2016",
+            start_time: "09:45",
+            end_time: "14:00",
+            project_name: "TL",
+            description: "development",
+        }.to_json(),
+        TaskRecord {
+            date: "09/19/2016",
+            start_time: "14:30",
+            end_time: "15:30",
+            project_name: "TL",
+            description: "aijsdf;lkjsdf;lkjs;flkja;lkfj;aslkfj;alksjf;laksjf;lkasjf;lkajsf;lkjsaflkjasflkjas;fkja;lkn;alvi;alkn;flkansf;lkjasdf;lkasd;flknq;oifj;qowif;okns;kan;sdkfna;lskfdnm;alkfn;kasfv;lknsmf;lkasf;lkjf;lkajfd;lkanmsf;lkajsf;lkansf;dlkajsf;lkam;lkansd;flkanf",
+        }.to_json(),
+    ]);
+    data
 }
